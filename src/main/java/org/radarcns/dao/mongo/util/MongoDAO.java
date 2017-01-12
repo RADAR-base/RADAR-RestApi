1793c0ecbb1ee4387601ff79a6fa9e2af0f64f38
Francesco Nobilia <f.nobilia@gmail.com>
2017-01-12 13:13:32 +0000

    Removed unused logger

--- src/main/java/org/radarcns/dao/mongo/util/MongoDAO.java
+++ src/main/java/org/radarcns/dao/mongo/util/MongoDAO.java
@@ -18,15 +18,13 @@ import javax.servlet.ServletContext;
 import org.bson.Document;
 import org.radarcns.config.Properties;
 import org.radarcns.listner.MongoDBContextListener;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 /**
  * Created by Francesco Nobilia on 20/10/2016.
  */
 public class MongoDAO {
 
-    private static final Logger logger = LoggerFactory.getLogger(MongoDAO.class);
+//    private static final Logger logger = LoggerFactory.getLogger(MongoDAO.class);
 
     private static final String USER = "user";
     private static final String SOURCE = "source";
