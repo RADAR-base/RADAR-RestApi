0ed0fd0b8918b131fa504d31cfaf1d2403065c20
yatharthranjan <yatharthranjan89@gmail.com>
2017-11-14 14:26:34 +0000

    Add custom AccessDenied Exception class

--- src/main/java/org/radarcns/security/utils/SecurityUtils.java
+++ src/main/java/org/radarcns/security/utils/SecurityUtils.java
@@ -1,10 +1,10 @@
 package org.radarcns.security.utils;
 
 import com.auth0.jwt.interfaces.DecodedJWT;
+import org.radarcns.security.exception.AccessDeniedException;
 import org.radarcns.security.filter.AuthenticationFilter;
 
 import javax.servlet.ServletRequest;
-import java.nio.file.AccessDeniedException;
 
 /**
  * Utility class for Rest-API Security.
