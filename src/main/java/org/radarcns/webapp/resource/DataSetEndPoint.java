a6b2d4c554823cee47fa61e84a8b71795fd57898
nivethika <nivemaham@gmail.com>
2018-02-08 11:41:29 +0100

    query parameter for time window parameters

--- src/main/java/org/radarcns/webapp/resource/DataSetEndPoint.java
+++ src/main/java/org/radarcns/webapp/resource/DataSetEndPoint.java
@@ -31,7 +31,6 @@ import static org.radarcns.webapp.resource.Parameter.STAT;
 import static org.radarcns.webapp.resource.Parameter.SUBJECT_ID;
 import static org.radarcns.webapp.resource.Parameter.TIME_WINDOW;
 
-import com.mongodb.MongoClient;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import java.io.IOException;
@@ -42,8 +41,8 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import org.radarcns.auth.NeedsPermissionOnSubject;
-import org.radarcns.catalog.SourceCatalog;
 import org.radarcns.domain.restapi.TimeWindow;
 import org.radarcns.domain.restapi.dataset.Dataset;
 import org.radarcns.domain.restapi.header.DescriptiveStatistic;
@@ -61,18 +60,12 @@ import org.slf4j.LoggerFactory;
  */
 @Authenticated
 @Path("/" + DATA)
-public class SensorEndPoint {
+public class DataSetEndPoint {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(SensorEndPoint.class);
-
-    @Inject
-    private MongoClient mongoClient;
+    private static final Logger LOGGER = LoggerFactory.getLogger(DataSetEndPoint.class);
 
     @Inject
     private ManagementPortalClient mpClient;
-
-    @Inject
-    private SourceCatalog sourceCatalog;
 
     @Inject
     private DataSetService dataSetService;
@@ -162,76 +155,27 @@ public class SensorEndPoint {
             @Alphanumeric @PathParam(SOURCE_ID) String sourceId,
             @Alphanumeric @PathParam(SOURCE_DATA_NAME) String sourceDataName,
             @PathParam(STAT) DescriptiveStatistic stat,
-            @PathParam(TIME_WINDOW) TimeWindow interval) throws IOException {
+            @PathParam(TIME_WINDOW) TimeWindow interval,
+            @QueryParam(START) Long start,
+            @QueryParam(END) Long end) throws IOException {
         // todo: 404 if given source does not exist.
         // Note that a source doesn't necessarily need to be linked anymore, as long as it exists
         // and historical data of it is linked to the given user.
         mpClient.getSubject(subjectId);
-
-        Dataset dataset = dataSetService.getAllDataItems(projectName, subjectId,
-                sourceId, sourceDataName, stat, interval);
+        Dataset dataset;
+        if(start != null && end != null) {
+            dataset = dataSetService.getAllRecordsInWindow(projectName,
+                    subjectId, sourceId, sourceDataName, stat, interval, start, end);
+        }
+        else {
+            dataset = dataSetService.getAllDataItems(projectName, subjectId,
+                    sourceId, sourceDataName, stat, interval);
+        }
 
         if (dataset.getDataset().isEmpty()) {
             LOGGER.debug("No data for the subject {} with source {}", subjectId, sourceId);
             return emptyDataset(projectName, subjectId, sourceId, sourceDataName, stat, interval,
                     Instant.now());
-        }
-
-        return dataset;
-
-    }
-
-    //--------------------------------------------------------------------------------------------//
-    //                                 WINDOWED-DATA FUNCTIONS                                    //
-    //--------------------------------------------------------------------------------------------//
-
-    /**
-     * JSON function that returns all data value inside the time-window [start-end].
-     */
-    @GET
-    @Produces({APPLICATION_JSON, AVRO_BINARY})
-    @Path("/{" + SOURCE_DATA_NAME + "}/{" + STAT + "}/{" + TIME_WINDOW + "}/{" + PROJECT_NAME
-            + "}/{"
-            + SUBJECT_ID + "}/{"
-            + SOURCE_ID + "}/{" + START + "}/{" + END + "}")
-    @Operation(summary = "Returns a dataset object formatted in JSON.",
-            description = "Each collected sample is aggregated to provide near real-time "
-                    + "statistical "
-                    + "results. This end-point returns all available results of type stat for the "
-                    + "given subjectID, sourceID, and sensor belonging to the time window "
-                    + "[start - end]. Data can be queried using different time-frame resolutions. "
-                    + "The response is formatted in JSON.")
-    @ApiResponse(responseCode = "500", description = "An error occurs while executing, in the body "
-            + "there is a message.avsc object with more details.")
-    @ApiResponse(responseCode = "200", description = "Returns a dataset.avsc object containing "
-            + "samples "
-            + "belonging to the time window [start - end] for the given inputs formatted "
-            + "either Acceleration.avsc or DoubleValue.avsc.")
-    @ApiResponse(responseCode = "401", description = "Access denied error occurred")
-    @ApiResponse(responseCode = "403", description = "Not Authorised error occurred")
-    @ApiResponse(responseCode = "404", description = "Subject not found.")
-    @NeedsPermissionOnSubject(entity = MEASUREMENT, operation = READ)
-    public Dataset getSamplesWithinWindowJson(
-            @Alphanumeric @PathParam(SOURCE_DATA_NAME) String sourceDataName,
-            @PathParam(STAT) DescriptiveStatistic stat,
-            @Alphanumeric @PathParam(PROJECT_NAME) String projectName,
-            @Alphanumeric @PathParam(SUBJECT_ID) String subjectId,
-            @Alphanumeric @PathParam(SOURCE_ID) String sourceId,
-            @PathParam(TIME_WINDOW) TimeWindow interval,
-            @PathParam(START) long start,
-            @PathParam(END) long end) throws IOException {
-        // todo: 404 if given source does not exist.
-        // Note that a source doesn't necessarily need to be linked anymore, as long as it exists
-        // and historical data of it is linked to the given user.
-        mpClient.getSubject(subjectId);
-
-        Dataset dataset = dataSetService.getAllRecordsInWindow(projectName,
-                subjectId, sourceId, sourceDataName, stat, interval, start, end);
-
-        if (dataset.getDataset().isEmpty()) {
-            LOGGER.debug("No data for the subject {} with source {}", subjectId, sourceId);
-            return emptyDataset(projectName, subjectId, sourceId, sourceDataName, stat, interval,
-                    Instant.ofEpochMilli(start));
         }
 
         return dataset;
