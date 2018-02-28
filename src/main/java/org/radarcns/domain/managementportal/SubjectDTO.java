2a494e14a86b18828d41eb19ee1d67fb1a1d4c99
nivethika <nivemaham@gmail.com>
2018-02-28 15:35:30 +0100

    remove unused imports

--- src/main/java/org/radarcns/domain/managementportal/SubjectDTO.java
+++ src/main/java/org/radarcns/domain/managementportal/SubjectDTO.java
@@ -19,7 +19,6 @@ package org.radarcns.domain.managementportal;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonSetter;
-import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
